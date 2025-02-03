ad2c308fd5a0556bd815ea1d2a4e878066940f36
namne3 <johan.callert@me.com>
2025-02-03 13:14:27 +0100

    Fuck you

---
+++ Assets/Code/quit.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    public void Quit()
+    {
+        //Du quittar unity :D YIPPI. JAG ÄR FRI!
+        Application.Quit();
+        Debug.Log("Fuck you");
+    }
+}
+//Kod av Johan
